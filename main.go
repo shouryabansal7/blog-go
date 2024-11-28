4f5f29a2c761c108d8e98428e2a0491e2eb05f8a
Shourya Bansal <shouryab2001@gmail.com>
2024-11-28 22:32:15 +0530

    initialize and setup the initial server

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main(){
+	fmt.Println("hello world")
+
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	srv := &http.Server{
+		Handler: router,
+		Addr: ":"+portString,
+	}
+
+
+	log.Printf("Server Starting on port %v", portString)
+	err := srv.ListenAndServe()
+	if err!=nil{
+		log.Fatal(err)
+	}
+
+
+	fmt.Println("Port:",portString)
+}
